9c802569b8b655020969a122f17b40c4e2e10d95
Javier Carrillo <stringparser@gmail.com>
2014-08-10 18:21:49 +0200

    0.1.x patches -> lexer.js word:word no split!

--- pool/index.js
+++ pool/index.js
@@ -1,39 +1,15 @@
+var gulp = require('gulp');
+var runtime = require('gulp-runtime');
 
-var util = require('../lib/utils');
+gulp.task('develop', function () {
 
-var runtime = require('../lib/runtime').Runtime('gulp');
+});
 
+gulp.task('jade:develop', function(){
 
-/*runtime.set('hello', function hello(argv, args, next){
-
-  console.log('---------------------')
-  console.log('\n Hello! \n');
-  console.log('---------------------')
-
-}).set('world', function world(){
-  console.log('---------------------')
-  console.log('\n Hello! \n consumed : ', 'world');
-  console.log('---------------------')
-}).handle(function overrridenWorld(){
-  console.log('---------------------')
-  console.log('\n Hello! \n consumed : ', 'overrridenWorld');
-  console.log('---------------------')
 })
 
-runtime.set('something', function something(argv, args, next){
-  console.log('---------------------')
-  console.log('\n Something! \n');
-  console.log('---------------------')
-}).set('else', function Else(){
-
-  console.log('---------------------')
-  console.log('\n Something! \n consumed : ', 'else');
-  console.log('---------------------')
-})
-
-runtime.handle(function rootHandle(){
-  console.log('root handlerrr')
-}).completion(['one', 'two', 'three']).completion(function(){
-  return [1,2,3]
-})*/
-
+// build everything and open main entry page
+gulp.task('default', ['develop'], function () {
+    // ...
+});
