df2a178e62f1f14a4ba3beb79458136a82c797bb
Javier Carrillo <stringparser@gmail.com>
2014-08-11 14:12:34 +0200

    0.1.x -> Warehouse not in Runtime anymore, you choose

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
  */
 var resolve = require('path').resolve;
 
+if(process.env.DEV_ENV){
+  console.log('> Careful! Using dev code:', resolve(process.cwd(),'lib/runtime'), '\n')
+}
 /*
  * Export depending on `env`
  */
 module.exports = process.env.DEV_ENV
  ? require( resolve(process.cwd(),'lib/runtime') )
- : require('./lib/runtime')
+ : require('./lib/runtime');
