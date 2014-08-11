09533765489b45341bfd681f650c87c16d5df989
Javier Carrillo <stringparser@gmail.com>
2014-08-11 12:41:25 +0200

    0.1.x -> DEV_ENV set

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+ * Module dependencies
+ */
+var resolve = require('path').resolve;
+
+/*
+ * Export depending on `env`
+ */
 module.exports = process.env.DEV_ENV
  ? require('./lib/runtime')
- : require('runtime')
+ : require( resolve(process.cwd(),'lib/runtime') )
