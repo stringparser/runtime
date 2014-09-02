7d736f7b3c9804b3dc499540477489a9b20e57bd
Javier Carrillo <stringparser@gmail.com>
2014-09-02 11:09:47 +0200

    0.1.33 -> lib/command.get if argument is string anchor will prevail

--- test/test.Command.js
+++ test/test.Command.js
@@ -13,17 +13,17 @@ test.forEach(function(testName, index){
 
   runtime[testName] = (new require('../lib/runtime')).create(testName);
 
-  runtime[testName]({ nested : false })
+  runtime[testName]
     .set('1', function(){ return this; })
     .set('2', function(){ return this.get('2'); })
     .set('3', function(){ return this.get('3'); })
     .set('4', function(){ return this.get('4'); });
 
-  runtime[testName]({ nested : true })
+  runtime[testName]
     .set('1-nest', function(){ return this.get('1-nest'); })
-    .set('2-nest', function(){ return this.get('2-nest'); })
-    .set('3-nest', function(){ return this.get('3-nest'); })
-    .set('4-nest', function(){ return this.get('4-nest'); });
+    .set('1-nest 2-nest', function(){ return this.get('2-nest'); })
+    .set('1-nest 2-nest 3-nest', function(){ return this.get('3-nest'); })
+    .set('1-nest 2-nest 3-nest 4-nest', function(){ return this.get('4-nest'); });
 
   runtime[testName]
     .set(['1-alias', '2-alias', '3-alias', '4-alias'], function(){ return this; });
