aa31b62f1c39884d3f844a18d00f4dff010b6cde
Javier Carrillo <stringparser@gmail.com>
2014-09-12 19:16:14 +0200

    consumer only runs once for each command

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-/*
- * Module dependencies
- */
-var resolve = require('path').resolve;
-
-if(process.env.DEV_ENV){
-  console.log('> Careful! Using dev code:', resolve(process.cwd(),'lib/runtime'), '\n')
-}
-/*
- * Export depending on `env`
- */
-module.exports = process.env.DEV_ENV
- ? require( resolve(process.cwd(),'lib/runtime') )
- : require('./lib/runtime');
+module.exports = require('./lib/runtime');
