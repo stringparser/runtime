dbe92b03d8a8463702089f3c4b449e69ec6b67eb
Javier Carrillo <stringparser@gmail.com>
2014-11-25 00:21:48 +0100

    no events, just commands

--- context.js
+++ context.js
@@ -5,13 +5,14 @@ var runtime = require('./.').create('context', {
     output : process.stdout
   });
 
-runtime.set('hello', function(){
-  [].slice.call(arguments).forEach(function(arg){
-    console.log(arg);
+runtime.set('ctx', function(ctx){
+  Object.keys(ctx).forEach(function(key){
+    console.log(key, ctx[key]);
   });
 });
 
 runtime.set('pass', function(ctx, args, next){
+  console.log('pass', arguments);
   next('args', ctx.argv, ctx.args, ctx.params);
 });
 
