b7434148a1b4c72acb9e3a346a7ab0f23279e1f6
Javier Carrillo <stringparser@gmail.com>
2014-12-10 01:23:35 +0100

    manifold needs to handle parameters for paths, after that it should be done

--- context.js
+++ context.js
@@ -6,34 +6,40 @@ var runtime = require('./.').create('context', {
     completion : null
   });
 
-runtime.set(function rootNode(next){
-  console.log('ctx root');
+runtime.set(function rootNode(err, next){
   console.log(next);
+  this.mode = 'parllel';
   if(next.done){ runtime.prompt(); }
 });
 
-runtime.set('series', function series(next){
-  next.async = true;
-  console.log('starting series with %s', next.argv.slice(next.index));
+runtime.set('series', function series(err, next){
+  next.wait = true;
+  this.mode = 'series';
+  console.log('\nStarting <%s> with %s',
+    this.mode, next.argv.slice(next.index));
   next();
 });
 
-runtime.set('parallel', function series(next){
-  next.async = false;
-  console.log('starting parallel with %s', next.argv.slice(next.index));
+runtime.set('parallel', function parallel(err, next){
+  next.wait = false;
+  this.mode = 'parallel';
+  console.log('\nStarting <%s> with %s',
+    this.mode, next.argv.slice(next.index));
   next();
 });
 
 var series = ['one', 'two', 'three'];
 series.forEach(function(name){
-  runtime.set(name, function(next){
+  runtime.set(name, function(err, next){
     var scope = next.clone(true);
     var rtime = Math.random()*1000;
-    console.log('"%s" in %s with "%s" ',
-      name, next.argv[0], next.argv.slice(next.index+1).join(', '));
+    var pending = next.argv.slice(next.index+1).join(', ');
+    console.log('`%s` in <%s> with `%s`',
+      name, this.mode || 'parallel', pending);
     setTimeout(function(){
       console.log('`%s` done after %s (%s)', name, scope.time(), rtime);
       console.log(scope);
+      console.log();
       next();
     }, rtime);
   });
