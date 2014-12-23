12865626904a440df95494c8153b5bfdea44a04e
Javier Carrillo <stringparser@gmail.com>
2014-12-24 00:22:07 +0100

    times measures! - Runtime.next => next function holds the state of the run timeline - ctx (at Runtime.next => next) should be a snapshot, not confident about performance at the moment with this, the context of the handle function that gets called is merge({}, ctx) so I should think of a better way

--- context.js
+++ context.js
@@ -1,37 +1,44 @@
 'use strict';
-
-var runtime = require('./.').create('context').repl({
+var argv = process.argv.slice(2);
+var app = require('./.').create('context').repl({
      input : process.stdin,
     output : process.stdout,
     completion : null
   });
 
-runtime.set(function rootNode(next){
-  this.mode = 'parallel';
-  if(next.done){ runtime.prompt(); }
+app.set(function rootNode(next){
+  if(next.done){ app.prompt(); }
 });
 
-runtime.set('series', function series(next){
-  console.log('\nStarting <%s> in series', next.argv.slice(next.index));
-  next.series = true; next();
-});
-
-runtime.set('parallel', function parallel(next){
-  console.log('\nStarting <%s> in parallel', next.argv.slice(next.index));
-  next.series = false; next();
-});
-
-runtime.set(':name(\\w+)', function(next){
-  var ctx = next.clone();
-  var name = next.params.name;
-  var rtime = Math.random()*1000;
-  setTimeout(function(){
-    console.log('`%s` done after %s (%s)', name, ctx.time(), rtime);
-    next();
-  }, rtime);
-});
-
-
-runtime.set('get page.data /url', function(next){
+app.set('#report', function(next){
+  var ctx = this;
+  console.log(ctx);
   console.log(next);
 });
+
+app.set('series', function series(next){
+  console.log('\nStarting <%s> in series', next.argv.slice(next.index).join(', '));
+  next.wait = true; next();
+  if(next.done){ next.time(next.argv.slice(0, next.index-next.done).join(' ')); return console.log(next); }
+  next.argv.push('series');
+});
+
+app.set('parallel', function parallel(next){
+  next.wait = false;
+  console.log('\nStarting <%s> in parallel', next.argv.slice(next.index).join(', '));
+  next.argv.push('parallel');
+  next();
+});
+
+app.set(':name(\\w+)', function(next){
+  var self = this;
+  var name = this.params.name;
+  process.nextTick(function(){
+    console.log('`%s` done after %s', name, next.time(self.path));
+    console.log(next);
+    next();
+  });
+});
+
+console.log(argv);
+if(argv.length){ app.next(argv)(); }
