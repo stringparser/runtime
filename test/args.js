4f1f177211d658687f3ddbfcf8ab204219a58026
Javier Carrillo <stringparser@gmail.com>
2015-01-26 23:46:20 +0100

    tests for argument passing between stems of the stack

---
+++ test/args.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var should = require('should');
+
+module.exports = function(runtime){
+  should.exists(runtime);
+  var app = runtime.create('args');
+
+  var logger = app.log.get().handle;
+  app.log.set(function(){}); // disable logging
+
+  it('should pass arguments around', function(done){
+    function one(next, foo, bar, baz){
+      next.wait = true;
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next();
+    }
+
+    function two(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next();
+    }
+
+    function three(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(); done();
+    }
+
+    app.next(one, two, three)(1, 2, 3);
+  });
+
+  it('should be able change arguments around', function(done){
+    function one(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(null, 2, 3, 4);
+    }
+
+    function two(next, foo, bar, baz){
+      foo.should.be.eql(2);
+      bar.should.be.eql(3);
+      baz.should.be.eql(4);
+      next(null, 5, 6, 7);
+    }
+
+    function three(next, foo, bar, baz){
+      foo.should.be.eql(5);
+      bar.should.be.eql(6);
+      baz.should.be.eql(7);
+      next(); done();
+    }
+
+    app.next(one, two, three)(1, 2, 3);
+  });
+
+  it('should not change arguments if length is less than 2', function(done){
+    function one(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(null);
+    }
+
+    function two(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(null);
+    }
+
+    function three(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      done();
+    }
+
+    app.next(one, two, three)(1, 2, 3);
+  });
+
+  it('should pass arguments around between stacks', function(done){
+    function one(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(null);
+    }
+
+    function two(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      next(null);
+    }
+
+    function three(next, foo, bar, baz){
+      foo.should.be.eql(1);
+      bar.should.be.eql(2);
+      baz.should.be.eql(3);
+      done();
+    }
+
+    app.next(app.next(one), app.next(two), app.next(three))(1, 2, 3);
+  });
+};
