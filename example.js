51e9528d98f2cc0ae2a7a8b15ea3a469ea597a5e
Javier Carrillo <stringparser@gmail.com>
2015-03-05 17:07:13 +0100

    smoke tests for posible stack combinations

--- example.js
+++ example.js
@@ -10,4 +10,8 @@ app.set(':handle(\\d+)', function(next){
   setTimeout(next, Math.random()*10);
 });
 
-app.stack(app.stack('1 2 3', {wait: true}), '4 5 6', {wait: true})(1,2,3);
+app.stack(
+  app.stack('1 2 3', {wait: true}),
+  '4 5 6',
+  {wait: true}
+)(1,2,3);
