b72ac3a397bf4d349fe91c0c2472493152f65276
Javier Carrillo <stringparser@gmail.com>
2015-03-06 21:50:05 +0100

    smoke test for the repl

--- example.js
+++ example.js
@@ -1,18 +1,17 @@
 'use strict';
 
 var runtime = require('./.');
-var should = require('should');
 var app = runtime.create('app');
-
-should.exists(app);
 
 app.set(':handle', function(next){
   setTimeout(next, Math.random()*10);
 });
 
-app.stack(
-  app.stack('1 2 3', {wait: true}),
-  app.stack('4 5 6',
-    app.stack('7 8 9', {wait: true}),
-  {wait: true}),
-{wait: true})(1,2,3);
+// app.stack(
+//   app.stack('1 2 3', {wait: true}),
+//   app.stack('4 5 6',
+//     app.stack('7 8 9', {wait: true}),
+//   {wait: true}),
+// {wait: true})(1,2,3);
+
+console.log(app);
