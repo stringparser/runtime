c3997e0b8a01f73ac214359ea8d533484b63e8b2
Javier Carrillo <stringparser@gmail.com>
2015-03-09 13:27:19 +0100

    smoke tests for the repl

--- example.js
+++ example.js
@@ -3,16 +3,15 @@
 var runtime = require('./.');
 var app = runtime.create('app');
 
-app.set('handle', function(next){
+app.set(':handle', function(next){
   setTimeout(next, Math.random()*10);
 });
 
-// app.stack(
-//   app.stack('1 2 3', {wait: true}),
-//   app.stack('4 5 6',
-//     app.stack('7 8 9', {wait: true}),
-//   {wait: true}),
-// {wait: true})(1,2,3);
+;['one', 'two', 'three', 'four']
+  .forEach(function(item){
+    app.set(item, function(next){
+      setTimeout(next, Math.random()*10);
+    });
+  });
 
 app.repl();
-console.log(Object.keys(require));
