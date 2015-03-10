47395e44d200611d1afa2b1d3c130c9bc51769a1
Javier Carrillo <stringparser@gmail.com>
2015-03-10 23:17:51 +0100

    added tests for integration with other technologies on a separate file

---
+++ test/integration.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(runtime, util){
+  var app = runtime.create('resolve', {log: false});
+
+  before(function(done){
+    app.set({onError: done});
+    util.rimraf('dir', function(){
+      util.mkdirp('dir/src', function(){
+        fs.open('dir/src/file.js', 'w', done);
+      });
+    });
+  });
+
+  it('async-done enables waiting to gulp streams', function(done){
+    app.set({onError: done});
+
+    function src(next, src, dest){
+      next.wait = true;
+      function destFolder(){
+        return path.resolve(__dirname, dest);
+      }
+
+      return util.gulp.src(src)
+       .pipe(util.gulp.dest(destFolder))
+       .on('error', next);
+    }
+
+    function end(){ fs.stat('dir/dest', done); }
+    app.stack(src, end)('dir/src/*.js', 'dir/dest');
+  });
+
+  it('async-done enables waiting to through streams', function(done){
+    app.set({onError: done});
+    app.repl({input: util.through.obj()});
+
+    function wait(next){
+      next.wait = true;
+      fs.createReadStream('./dir/src/file.js').pipe(app.repl.input);
+      return app.repl.input;
+    }
+
+    function end(){ done(); }
+
+    app.stack(wait, end)();
+  });
+};
