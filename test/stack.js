562c61e1b61a1d539bf60f12c10275416112a31f
Javier Carrillo <stringparser@gmail.com>
2015-03-10 22:48:58 +0100

    moved test/stems.js to test/stack.js, adding repl tests

--- test/stack.js
+++ test/stack.js
@@ -3,6 +3,70 @@
 module.exports = function(runtime){
   var app = runtime.create('stack', {log: false});
 
+  it('should accept (separated, strings)', function(done){
+    app.set({onError: done});
+
+    app.set('1', function(next){
+      next.match.should.be.eql('1');
+      next.path.should.be.eql('1');
+    });
+
+    app.set('2', function(next){
+      next.match.should.be.eql('2');
+      next.path.should.be.eql('2');
+      done();
+    });
+
+    app.stack('1', '2')();
+  });
+
+  it('should accept (join strings argument)', function(done){
+    app.set({onError: done});
+
+    app.set('1', function(next){
+      next.match.should.be.eql('1');
+      next.path.should.be.eql('1 2');
+    });
+
+    app.set('2', function(next){
+      next.match.should.be.eql('2');
+      next.path.should.be.eql('2');
+      done();
+    });
+
+    app.stack('1 2')();
+  });
+
+  it('should accept (function, function)', function(done){
+    app.set({onError: done});
+
+    function one(next){
+      next.match.should.be.eql('one');
+    }
+
+    function two(next){
+      next.match.should.be.eql('two');
+      done();
+    }
+
+    app.stack(one, two)();
+  });
+
+  it('should accept (string, function)', function(done){
+    app.set({onError: done});
+
+    app.set('a :string', function(next){
+      next.match.should.be.eql('a word');
+    });
+
+    function two(next){
+      next.match.should.be.eql('two');
+      done();
+    }
+
+    app.stack('a word', two)();
+  });
+  
   it('should pass arguments around', function(done){
     app.set({onError: done});
 
