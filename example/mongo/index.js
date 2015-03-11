4b1d5a1600d5db43015f2feee6fdccf713b4d8a5
Javier Carrillo <stringparser@gmail.com>
2015-03-11 22:07:30 +0100

    less confusing example

--- example/mongo/index.js
+++ example/mongo/index.js
@@ -5,24 +5,23 @@ var db = mongodb('db', ['users']);
 var app = require('../../.').create('mongo-example');
 
 function remove(next){
-  this.remove({}, next);
+  db.users.remove({}, next);
 }
 
 function count(next){
-  this.count(next);
+  db.users.count(next);
 }
 
 function insert(next, result, user){
-  this.insert(user, next);
+  db.users.insert(user, next);
 }
 
 function find(next){
-  this.find(next);
+  db.users.find(next);
 }
 
 var query = app.stack(remove, count, insert, find, {
   wait: true,
-  context: db.users,
   onHandleEnd: function(next){
     this.results = this.results || [ ];
     this.results.push({
