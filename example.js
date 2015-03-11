a1e54a80798c5e1d3a07c897cbaad273ee2b4d3e
Javier Carrillo <stringparser@gmail.com>
2015-03-11 16:05:30 +0100

    move example to example/index.js

--- example.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var runtime = require('./.');
-var app = runtime.create('app');
-
-process.hrtime = require('browser-process-hrtime');
-
-window.require = require;
-window.runtime = runtime;
