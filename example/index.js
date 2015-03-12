dcb5c5971faba0e39543d18b68c3c877883f159a
Javier Carrillo <stringparser@gmail.com>
2015-03-12 12:36:24 +0100

    lint

--- example/index.js
+++ example/index.js
@@ -4,12 +4,8 @@ var runtime = require('../.');
 var app = runtime.create('app');
 
 app.set(':handle', function(next){
-  setTimeout(next, Math.random()*10);
+  setTimeout(next, Math.random()*2000);
 });
 
 
-app.stack(
-  '1 2 3',
-  app.stack('4 5 6', app.stack('7 8 9', {wait: true}), {wait: true}),
-  {wait: true}
-)();
+app.stack('1 2 3', app.stack('4 5 6', app.stack('7 8 9')))();
