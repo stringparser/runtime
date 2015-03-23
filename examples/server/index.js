92ab6c5bf24f04ce09ac62af609824466e4fd5ae
Javier Carrillo <stringparser@gmail.com>
2015-03-23 17:23:32 +0100

    update

--- examples/server/index.js
+++ examples/server/index.js
@@ -3,6 +3,14 @@
 var http = require('http');
 var app = require('../../.').create('myAppName');
 
+app.set({
+  onHandleNotFound: function(next, req, res){
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('404: There is no path \''+req.url+'\' defined yet.');
+    next();
+  }
+});
+
 app.set('get /', app.stack(index, query, end));
 
 function index(next, req, res){
@@ -12,7 +20,7 @@ function index(next, req, res){
 
 function query(next, req, res){
   var name = req.url.match(/\?name=([^&]+)/);
-  var user = name ? '<i>' + name[1] + '</i>' : '"anonymous"';
+  var user = name ? name[1] : '"anonymous"';
   res.write(user);
   return res;
 }
@@ -25,20 +33,6 @@ function router(req, res){
   var method = req.method.toLowerCase();
   app.stack(method + ' '+ req.url)(req, res);
 }
-
-app.set({
-  onHandleCall: function(next, req, res){
-    res.write('<!DOCTYPE html><html>');
-  },
-  onHandleNotFound: function(next, req, res){
-    res.writeHead(404);
-    res.end('404: There is no \''+req.url+'\' path defined yet.');
-    next();
-  },
-  onHandleEnd: function(next, req, res){
-    res.end('</html>');
-  }
-});
 
 http.createServer(router).listen(8000, function(){
   console.log('http server running on port 8000');
