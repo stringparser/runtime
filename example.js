ea716747714e01a0096988750dccbaea9e64fafc
Javier Carrillo <stringparser@gmail.com>
2015-08-12 13:21:06 +0200

    dev: smoke tests before real testing

--- example.js
+++ example.js
@@ -1,24 +1,25 @@
 'use strict';
 
-var runtime = require('./.');
+var runtime = require('./.').create({
+  onHandle: function(){
+    console.log('onHandle');
+    console.log(arguments);
+    console.log('--');
+  }
+});
 
 function one(next){
-  var stack = this;
-  console.log('one start\n', stack);
   setTimeout(function(){
-    console.log('one end\n', stack);
     next(null, 3, 4);
   }, 1000);
 }
 
 function two(next){
-  console.log('two start', this);
   next();
-  console.log('two end', this);
 }
 
-runtime(one, two, function(next){ next(); },
-  runtime(one, two, {wait: true}))(1, 2, function end(){
+runtime.stack(one, two, function(next){ console.log('anonymous'); next(); },
+  runtime.stack(one, two, {wait: true}))(1, 2, function end(){
     console.log('ended!');
     console.log(arguments);
   });
