22ee80eb91b7e379e7ed09053b3319e6843bad2e
Javier Carrillo <stringparser@gmail.com>
2015-08-12 23:09:38 +0200

    tests: fix tests log

--- test/index.js
+++ test/index.js
@@ -5,9 +5,9 @@ require('should');
 var fs = require('fs');
 var pack = require('../package.json');
 
-fs.readdirSync(__dirname).forEach(function(file){
-  if(file === 'index.js'){ return; }
-  describe(pack.name, function(){
+describe(pack.name, function(){
+  fs.readdirSync(__dirname).forEach(function(file){
+    if(file === 'index.js'){ return; }
     describe(file.split('.')[0], function(){
       require('./' + file);
     });
