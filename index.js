a7088048d9a6212292d6cb766e93896ff9c4e605
Javier Carrillo <stringparser@gmail.com>
2015-08-12 13:23:12 +0200

    dev: compose -> stack method name

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var util = require('./lib/util');
 var Runtime = require('./lib/Runtime');
 
 exports = module.exports = util.merge(
-  Runtime.prototype.compose.bind(new Runtime()), {
+  Runtime.prototype.stack.bind(new Runtime()), {
     Stack: require('./lib/Stack'),
     create: create,
     Runtime: Runtime,
   }
 );
 
-function create(name, mixin){
+function create(name, props){
   if(create.store[name]){ return create.store[name]; }
 
-  mixin = mixin || name;
+  props = props || name;
   if(typeof name !== 'string'){ name = util.stringID(); }
-  return create.store[name] = new Runtime(mixin);
+  return create.store[name] = new Runtime(props);
 }
-create.store = Object.create(null);
+create.store = {};
