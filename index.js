d507ae1ee76f426543c55a9a0c4bd0ab06470506
Javier Carrillo <stringparser@gmail.com>
2015-08-15 12:41:19 +0200

    dev: moved main code to index.js

--- index.js
+++ index.js
@@ -1,31 +1,99 @@
 'use strict';
 
-var util = require('./lib/util');
-var Runtime = require('./lib/Runtime');
+var util = require('./util');
+var Stack = require('./Stack');
 
-/*
- Missing docs
-*/
 exports = module.exports = Runtime;
 
 /*
- Missing docs
-*/
-exports.create = function create(props){
+  Missing docs
+**/
+function Runtime(props){
+  if(!(this instanceof Runtime)){
+    return new Runtime(props);
+  }
+
+  util.merge(this, util.type(props).plainObject);
+}
+
+/**
+  Missing docs
+ */
+Runtime.create = function create(props){
   return new Runtime(props);
 };
 
+Runtime.extend = util.classFactory(Runtime);
+Runtime.prototype.Stack = Runtime.Stack = Stack;
+
 /*
- Missing docs
-*/
-exports.createClass = exports.extend = function createClass(mixin){
-  function RuntimeConstructor(props){
-    if(!(this instanceof RuntimeConstructor)){
-      return new Runtime(props);
+  Missing docs
+**/
+Runtime.prototype.onHandle = function(){ };
+Runtime.prototype.onHandleError = function(err){ throw err; };
+
+/*
+  Missing docs
+**/
+Runtime.prototype.stack = function(/* functions... */){
+  var fns = arguments;
+  var self = this;
+
+  composer.stack = new self.Stack(fns);
+  // ^ mark the composer function so it can be identified
+  function composer(handle, host, next){
+    var stack = new self.Stack(fns, true);
+    if(handle === composer){
+      stack.host = host;
+      stack.args = host.wait && host.args || [];
+      stack.callback = next;
+    } else {
+      stack.args = util.slice(arguments);
+      stack.callback = typeof arguments[arguments.length-1] === 'function' &&
+        stack.args.pop() || self.onHandleError;
     }
-    Runtime.apply(this, arguments);
+    return tick(stack);
   }
-  util.inherits(RuntimeConstructor, Runtime);
-  util.merge(RuntimeConstructor.prototype, util.type(mixin).plainObject);
-  return RuntimeConstructor;
+
+  // runs each handle
+  function tick(stack){
+    var handle = stack[stack.index];
+    var args = handle.stack instanceof Stack
+      ? [handle, stack, next]
+      : [next].concat(stack.args);
+
+    next.wait = Boolean(stack.wait);
+    stack.next = ++stack.index < stack.length;
+
+    util.asyncDone(function onNext(){
+      self.onHandle(next, handle, stack);
+      var result = handle.apply(stack.context || self, args);
+      if(next.wait){ return result; }
+      if(stack.next){ tick(stack); }
+      if(stack.host.next){ tick(stack.host); }
+      return result;
+    }, next);
+
+    function next(err){
+      if(err instanceof Error){
+        stack.callback.apply(self, [err].concat(args));
+      }
+
+      if(next.wait && arguments.length){
+        util.mapPush(stack.args, arguments, err instanceof Error ? 1 : 0);
+      }
+
+      next.end = true;
+      stack.splice(stack.indexOf(handle), 1);
+      self.onHandle(next, handle, stack);
+
+      if(stack.next){ tick(stack); }
+      else if(stack.host.next){ tick(stack.host); }
+      else if(!stack.length && stack.callback){
+        stack.callback.apply(self, [null].concat(stack.args));
+      }
+    }
+  }
+
+  return composer;
 };
