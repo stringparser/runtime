400d0ac3b3866df2b8184fd0f1aaa7bbb83d8557
Javier Carrillo <stringparser@gmail.com>
2015-08-15 21:29:23 +0200

    dev: organized and improved code
    - better argument passing from host
    - assign to stack.context once, not for each handle
    - --stack.index is necesary while cleaning each handle from the stack
    - clean the reference to the stack object once all is done so GC can take care of it

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var util = require('./util');
-var Stack = require('./Stack');
+var util = require('./lib/util');
+var Stack = require('./lib/Stack');
 
 exports = module.exports = Runtime;
 
-/*
-  Missing docs
+/**
+ Missing docs
 **/
 function Runtime(props){
   if(!(this instanceof Runtime)){
@@ -17,82 +17,82 @@ function Runtime(props){
 }
 
 /**
-  Missing docs
- */
-Runtime.create = function create(props){
-  return new Runtime(props);
-};
+ Missing docs
+**/
+Runtime.Stack = Stack;
+Runtime.create = function create(props){ return new Runtime(props); };
+Runtime.createClass = util.classFactory(Runtime);
 
-Runtime.extend = util.classFactory(Runtime);
-Runtime.prototype.Stack = Runtime.Stack = Stack;
-
-/*
+/**
   Missing docs
 **/
+Runtime.prototype.Stack = Stack;
 Runtime.prototype.onHandle = function(){ };
 Runtime.prototype.onHandleError = function(err){ throw err; };
 
-/*
+/**
   Missing docs
 **/
 Runtime.prototype.stack = function(/* functions... */){
   var fns = arguments;
   var self = this;
 
+  // just so composer is identifiable
   composer.stack = new self.Stack(fns);
-  // ^ mark the composer function so it can be identified
-  function composer(handle, host, next){
+  function composer(next, handle, host){
     var stack = new self.Stack(fns, true);
-    if(handle === composer){
+    if(handle === composer){ // we are inside other stack
       stack.host = host;
-      stack.args = host.wait && host.args || [];
+      stack.args = host.wait ? host.args : host.args.concat();
       stack.callback = next;
     } else {
+      stack.host = false;
       stack.args = util.slice(arguments);
-      stack.callback = typeof arguments[arguments.length-1] === 'function' &&
-        stack.args.pop() || self.onHandleError;
+      stack.callback = typeof arguments[arguments.length-1] === 'function'
+        ? stack.args.pop()
+        : self.onHandleError;
     }
+    stack.index = 0;
+    stack.context = stack.context || self;
     return tick(stack);
   }
 
   // runs each handle
   function tick(stack){
     var handle = stack[stack.index];
-    var args = handle.stack instanceof Stack
-      ? [handle, stack, next]
-      : [next].concat(stack.args);
-
-    next.wait = Boolean(stack.wait);
+    var args = handle.stack instanceof Stack ? [handle, stack] : stack.args;
     stack.next = ++stack.index < stack.length;
 
-    util.asyncDone(function onNext(){
+    var next;
+    util.asyncDone(function onNext(done){
+      args = [next = done].concat(args);
+      next.wait = Boolean(stack.wait);
       self.onHandle(next, handle, stack);
-      var result = handle.apply(stack.context || self, args);
+      var result = handle.apply(stack.context, args);
       if(next.wait){ return result; }
       if(stack.next){ tick(stack); }
       if(stack.host.next){ tick(stack.host); }
       return result;
-    }, next);
-
-    function next(err){
+    }, function onDone(err){
       if(err instanceof Error){
-        stack.callback.apply(self, [err].concat(args));
+        return stack.callback.call(self, err, handle, stack);
+      } else if(next.wait && arguments.length){
+        util.mapPush(stack.args, arguments,
+          util.type(err).match(/null|undefined|error/) ? 1 : 0
+        );
       }
 
-      if(next.wait && arguments.length){
-        util.mapPush(stack.args, arguments, err instanceof Error ? 1 : 0);
-      }
-
-      next.end = true;
+      next.end = true; --stack.index;
       stack.splice(stack.indexOf(handle), 1);
       self.onHandle(next, handle, stack);
 
       if(stack.next){ tick(stack); }
       else if(stack.host.next){ tick(stack.host); }
-      else if(!stack.length && stack.callback){
+      else if(!stack.length){
         stack.callback.apply(self, [null].concat(stack.args));
+        if(!stack.host){ stack = null; }
       }
-    }
+    });
   }
 
   return composer;
