ce2df3644fe212294c2e29d305ed753421a056e1
Javier Carrillo <stringparser@gmail.com>
2015-08-18 00:01:47 +0200

    dev: moved all prototype code of the Runtime to its own file

--- index.js
+++ index.js
@@ -1,101 +1,23 @@
 'use strict';
 
 var util = require('./lib/util');
-var Stack = require('./lib/Stack');
+var Runtime = require('./lib/Runtime');
 
 exports = module.exports = Runtime;
 
 /**
  Missing docs
 **/
-function Runtime(props){
-  if(!(this instanceof Runtime)){
-    return new Runtime(props);
-  }
-
-  util.merge(this, util.type(props).plainObject);
-}
+exports.Stack = require('./lib/Stack');
 
 /**
  Missing docs
 **/
-Runtime.Stack = Stack;
-Runtime.create = function create(props){ return new Runtime(props); };
-Runtime.createClass = util.classFactory(Runtime);
-
-/**
-  Missing docs
-**/
-Runtime.prototype.Stack = Stack;
-Runtime.prototype.onHandle = function(){ };
-Runtime.prototype.onHandleError = function(err){ throw err; };
-
-/**
-  Missing docs
-**/
-Runtime.prototype.stack = function(/* functions... */){
-  var fns = arguments;
-  var self = this;
-
-  // just so composer is identifiable
-  composer.stack = new self.Stack(fns);
-  function composer(next, handle, host){
-    var stack = new self.Stack(fns, true);
-    if(handle === composer){ // we are inside other stack
-      stack.host = host;
-      stack.args = host.wait ? host.args : host.args.concat();
-      stack.callback = next;
-    } else {
-      stack.host = false;
-      stack.args = util.slice(arguments);
-      stack.callback = typeof arguments[arguments.length-1] === 'function'
-        ? stack.args.pop()
-        : self.onHandleError;
-    }
-    stack.index = 0;
-    stack.context = stack.context || self;
-    return tick(stack);
-  }
-
-  // runs each handle
-  function tick(stack){
-    var handle = stack[stack.index];
-    stack.next = ++stack.index < stack.length;
-    var args = [next].concat(handle.stack instanceof Stack
-      ? [handle, stack] : stack.args
-    );
-
-    util.asyncDone(function onNext(){
-      next.wait = Boolean(stack.wait);
-      self.onHandle(next, handle, stack);
-      var result = handle.apply(stack.context, args);
-      if(next.wait){ return result; }
-      if(stack.next){ tick(stack); }
-      if(stack.host.next){ tick(stack.host); }
-      return result;
-    }, next);
-
-    function next(err){
-      if(err instanceof Error){
-        return stack.callback.call(self, err, handle, stack);
-      } else if(next.wait && arguments.length){
-        util.mapPush(stack.args, arguments,
-          util.type(err).match(/null|undefined|error/) ? 1 : 0
-        );
-      }
-
-      next.end = true; --stack.index;
-      stack.splice(stack.indexOf(handle), 1);
-      self.onHandle(next, handle, stack);
-
-      if(stack.next){ tick(stack); }
-      else if(stack.host.next){ tick(stack.host); }
-
-      if(!stack.length){
-        stack.callback.apply(self, [null].concat(stack.args));
-      }
-    }
-  }
-
-  return composer;
+exports.create = function create(props){
+  return new Runtime(props);
 };
+
+/**
+ Missing docs
+**/
+exports.createClass = util.classFactory(Runtime);
