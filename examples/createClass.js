e50f47cda7b56a907f7402251a1d5f9950fff400
Javier Carrillo <stringparser@gmail.com>
2015-12-09 23:58:04 +0100

    doc: example for createClass

--- examples/createClass.js
+++ examples/createClass.js
@@ -4,16 +4,20 @@ var Runtime = require('../.');
 
 var Tor = Runtime.createClass({
   create: function(){
-    this.isTor = true;
+    this.tor = true;
   }
 });
 
 var Tur = Tor.createClass({
-  create: function(){
-    this.thing = true;
+  create: function(props, SuperTor){
+    SuperTor.call(this, props);
+    this.tur = true;
   }
 });
 
-console.log(
-  Tur.create()
-)
+var tur = Tur.create({one: 1, two: 2});
+
+console.log(tur);
+console.log('tur instanceof Tur', tur instanceof Tur);
+console.log('tur instanceof Tor', tur instanceof Tor);
+console.log('tur instanceof Runtime', tur instanceof Runtime);
